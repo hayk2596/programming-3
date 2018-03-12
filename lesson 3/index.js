abe33c4cd6649b66d12813171690b59557236ead
Your Name <you@example.com>
2018-03-12 17:30:05 +0400

    df

---
+++ lesson 3/index.js
@@ -0,0 +1,16 @@
+// var Square = require("./module");
+// var mySquareObject = new Square(5);
+
+// function main(){
+//    console.log(mySquareObject.getArea());
+// }
+// main();
+
+
+// var fs = require('fs');
+
+// function main(){
+//    var file  = "hello.txt";
+//    fs.appendFileSync(file, "Hello world\n");
+// }
+// main();
